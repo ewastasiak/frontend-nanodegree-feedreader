6a965b1d867f9da3a9fea651532bc61df1354841
Ewa-Stasiak <ewa.stasiak.poczta@gmail.com>
2018-07-03 20:50:58 +0200

    docs: Add TODOs

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,24 +1,87 @@
 $(function() {
 
-// Suite I
-describe('RSS Feeds', function() {
 
-});
+  // Suite I
+  describe('RSS Feeds', function() {
 
-// Suite II
-describe('The menu', function() {
 
-});
+    //1/3
+    // Example from boilercode - it tests to make sure that the
+    // allFeeds variable has been defined and that it is not
+    // empty.
 
-// Suite III
-describe('Initial Entries', function() {
+    it('are defined', function() {
+        expect(allFeeds).toBeDefined();
+        expect(allFeeds.length).not.toBe(0);
+    });
 
-});
+    // 2/3
+    /* TODO: Write a test that loops through each feed
+     * in the allFeeds object and ensures it has a name defined
+     * and that the name is not empty.
+     */
 
-// Suite IV
-describe('New Feed Selection', function() {
+    // 3/3
+    /* TODO: Write a test that loops through each feed
+     * in the allFeeds object and ensures it has a URL defined
+     * and that the URL is not empty.
+     */
 
-});
+
+  });
+
+
+
+  // Suite II
+  describe('The menu', function() {
+
+
+    //1/2
+    /* TODO: Write a test that ensures the menu element is
+     * hidden by default. You'll have to analyze the HTML and
+     * the CSS to determine how we're performing the
+     * hiding/showing of the menu element.
+     */
+
+
+    //2/2
+     /* TODO: Write a test that ensures the menu changes
+      * visibility when the menu icon is clicked. This test
+      * should have two expectations: does the menu display when
+      * clicked and does it hide when clicked again.
+      */
+  });
+
+
+
+  // Suite III
+  describe('Initial Entries', function() {
+
+
+    //1/1
+    /* TODO: Write a test that ensures when the loadFeed
+     * function is called and completes its work, there is at least
+     * a single .entry element within the .feed container.
+     * Remember, loadFeed() is asynchronous so this test will require
+     * the use of Jasmine's beforeEach and asynchronous done() function.
+     */
+
+
+  });
+
+
+
+  // Suite IV
+  describe('New Feed Selection', function() {
+
+
+    //1/1
+    /* TODO: Write a test that ensures when a new feed is loaded
+     * by the loadFeed function that the content actually changes.
+     * Remember, loadFeed() is asynchronous.
+     */
+
+  });
 
 
 }());
